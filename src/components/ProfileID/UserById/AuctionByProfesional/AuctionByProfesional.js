20325d4947d42b4777b6d853c08c7894b4ac9709
Juan Prado <juan_prado_rojas@outlook.com>
2022-06-28 20:59:09 -0400

    Auctions

---
+++ src/components/ProfileID/UserById/AuctionByProfesional/AuctionByProfesional.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./AuctionByProfesional.css"
+
+
+function AuctionByProfesional({idPr}) {
+    console.log(idPr)
+
+
+
+
+  return (
+    <div className='containerAuctionByProfesional'>
+      AuctionByProfesional
+    </div>
+  )
+}
+
+export default AuctionByProfesional
