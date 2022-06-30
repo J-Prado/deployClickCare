a02e9fa89ddb2cdb70bc5365f6e8b7c123f736a0
Juan Prado <juan_prado_rojas@outlook.com>
2022-06-30 00:46:05 -0400

    About

--- src/components/About/Components/Phone/Phone.js
+++ src/components/About/Components/Phone/Phone.js
@@ -1,20 +1,32 @@
-import React from 'react'
-import phone from "./Img/62.png"
-import "./Phone.css"
+import React from "react";
+import phone from "./Img/62.png";
+import "./Phone.css";
 function Phone() {
   return (
-    <div className='phoneContainer'>
-    <div className='Colum1'></div>
-    <div className='phoneCall'>
-          <img className='phoneImg' src={phone} alt=""/>
+    <div className="phoneContainer">
+      <div className="Colum1"></div>
+      <div className="phoneCall">
+        <img className="phoneImg" src={phone} alt="" />
+      </div>
+      <div className="phoneTexto">
+        <p>
+          Descarga ya nuestra app para llevar la seguridad a todas partes en
+          todo momento.
+        </p>
+        <button className="buttonOne">
+          {" "}
+          <a
+            className="appOne"
+            href="https://expo.dev/@ryudark037/Mobile-Click-Care?serviceType=classic&distribution=expo-go"
+          >
+            {" "}
+            Descargar
+          </a>{" "}
+        </button>
+      </div>
+
+      <div className="Colum1"></div>
     </div>
-    <div className='phoneTexto'>
-      <p>Descarga ya nuestra app para llevar la seguridad a todas partes en todo momento.</p>
-      <button className='buttonOne'>Descargar</button>
-    </div>
-   
-    <div className='Colum1'></div>
-    </div>
-  )
+  );
 }
-export default Phone
+export default Phone;
